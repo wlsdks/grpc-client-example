6d4320a7a08a383441a7721a7e32979caad482ee
wlsdks <wlsdks12@naver.com>
2025-01-08 00:02:33 +0900

    :sparkles: 스프링에 feign 클라이언트 활성화

--- src/main/java/com/demo/grpc_client/GrpcClientApplication.java
+++ src/main/java/com/demo/grpc_client/GrpcClientApplication.java
@@ -2,12 +2,14 @@ package com.demo.grpc_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class GrpcClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcClientApplication.class, args);
+    }
 
 }
