97c8076ba175354e61d316ad1212482da579875d
wlsdks <wlsdks12@naver.com>
2025-01-10 21:54:23 +0900

    :sparkles: 예외 추가 (구현 필요함)

---
+++ src/main/java/com/demo/grpc_client/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.demo.grpc_client.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
---
+++ src/main/java/com/demo/grpc_client/exception/JwtAuthenticationException.java
@@ -0,0 +1,11 @@
+package com.demo.grpc_client.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+
+}
