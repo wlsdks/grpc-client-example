8a751b590c005300ef84b639620a0f6d475bf724
wlsdks <wlsdks12@naver.com>
2025-01-23 00:12:13 +0900

    :recycle: 서버 설정 생성자 바인딩으로 변경

--- src/main/java/com/demo/grpc_client/config/security/server/ServerProperties.java
+++ src/main/java/com/demo/grpc_client/config/security/server/ServerProperties.java
@@ -2,14 +2,18 @@ package com.demo.grpc_client.config.security.server;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
-@ConfigurationProperties(prefix = "server")  // application.yml의 'server' 프리픽스와 매칭
-@Component
+@ConfigurationProperties(prefix = "app.server")  // application.yml의 'server' 프리픽스와 매칭
 public class ServerProperties {
 
-    private ServerType type;  // server.type과 자동으로 매핑됨
-    private String id;  // server.id와 매핑됨
+    private final ServerType type;  // server.type과 자동으로 매핑됨
+    private final String id;  // server.id와 매핑됨
+
+    // 생성자 바인딩 사용
+    public ServerProperties(ServerType type, String id) {
+        this.type = type;
+        this.id = id;
+    }
 
 }
