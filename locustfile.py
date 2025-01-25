b98c65581722626ae292164617902409cb6e3e5b
wlsdks <wlsdks12@naver.com>
2025-01-25 13:34:00 +0900

    :recycle: locust에 task를 넣어서 원하는걸로 교체하면서 테스트 할 수 있도록 추가

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,22 @@
 from locust import HttpUser, task, between
 import json
 
+
 class GrpcVsFeignTest(HttpUser):
     wait_time = between(0.1, 0.5)
-    token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5hbkB0ZXN0LmNvbSIsImlhdCI6MTczNzcyNjY1MiwiZXhwIjoxNzM3NzMwMjUyfQ.XytkkongdksyVUk6kHXGVMzZCwyCdOETv8pLbR23l3A"
+    token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5hbkB0ZXN0LmNvbSIsImlhdCI6MTczNzc3MzIyOCwiZXhwIjoxNzM3Nzc2ODI4fQ.4jCCLyXzFOdPApfTSFYEALWeYXqd1C9XCYvhgJhbmjk"
 
     def on_start(self):
         self.client.headers = {"Authorization": self.token}
 
-    @task
+
+    @task(1)
+    # @task(0)
     def test_grpc(self):
         self.client.get("/api/test/grpc", params={"memberId": "1"}, name="gRPC Call")
 
-    @task
+
+    @task(0)
+    # @task(1)
     def test_feign(self):
         self.client.get("/api/test/feign", params={"memberId": "1"}, name="Feign Call")
