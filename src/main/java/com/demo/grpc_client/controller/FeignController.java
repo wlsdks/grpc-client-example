972d4178ea0e3b8c27417cfa6be724a3506e539b
wlsdks <wlsdks12@naver.com>
2025-02-14 15:45:06 +0900

    :recycle: 어노테이션 기반 서비스를 적용시킨 컨트롤러 추가

--- src/main/java/com/demo/grpc_client/controller/FeignController.java
+++ src/main/java/com/demo/grpc_client/controller/FeignController.java
@@ -2,6 +2,7 @@ package com.demo.grpc_client.controller;
 
 import com.demo.grpc_client.dto.response.ResponseMemberDTO;
 import com.demo.grpc_client.service.FindMemberService;
+import com.demo.grpc_client.service.FindMemberServiceAnnotation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeignController {
 
     private final FindMemberService findMemberService;
+    private final FindMemberServiceAnnotation findMemberServiceAnnotation;
 
     @GetMapping("/feign")
     public ResponseEntity<ResponseMemberDTO> feignTest(@RequestParam Long memberId) {
         log.trace("[FEIGN TEST] 들어온 HTTP 요청 - memberId={}", memberId);
 
         // 1) FeignClient로 원격 서버의 /api/members/{memberId} 호출
-        ResponseMemberDTO response = findMemberService.findMemberInformation(memberId);
+        ResponseMemberDTO response = findMemberServiceAnnotation.findMemberInformation(memberId);
 
         // 2) 최종 HTTP 응답
         return ResponseEntity.ok(response);
